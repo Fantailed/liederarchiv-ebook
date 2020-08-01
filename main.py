e0d276fc18517abafcbffd03ec7395a3c127a742
Conny Lin <cornelia.lin@tum.de>
2020-08-02 01:30:04 +0200

    Get basic epub output

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from bs4 import BeautifulSoup
 from collections import OrderedDict
 from ebooklib import epub
 from pathlib import Path
+from PIL import Image
 from string import ascii_lowercase as letters
+import io
 import requests
 
 
@@ -22,7 +24,7 @@ def get_song_list(list_url):
 
 
 def _get_song_title(song_soup):
-    return song_soup.find('div', class_='heading').find('h2').text
+    return song_soup.find('div', class_='heading').find('h2')
 
 
 def get_song_title(song_url):
@@ -71,15 +73,25 @@ def save_score_image(song_url):
 class Song:
     def __init__(self, song_url):
         song_soup = BeautifulSoup(requests.get(song_url).text, 'html.parser')
-        self.title = _get_song_title(song_soup)
+        self.title = _get_song_title(song_soup).text
+        self.title_html = str(_get_song_title(song_soup))
         self.score_img = save_score_image(song_url)
         self.text = _get_song_text(song_soup)
+        self.url = song_url
+        self.filename = song_url.split('/')[-1]
+        self.id = song_url.split('/')[-1].split('_')[-1].split('.')[0]
+        # Create song HTML
+        song_soup = BeautifulSoup(self.title_html, 'html.parser')
+        new_img = song_soup.new_tag('img', src=self.score_img)
+        song_soup.append(new_img)
+        song_soup.append(self.text)
+        self.html = str(song_soup)
 
     def __str__(self):
-        return f"==== SONG ====" \
-               f"Title: {self.title}" \
-               f"Sheet Music: {self.score_img}" \
-               f"Text: {self.text}"
+        return f"==== SONG ====\n" \
+               f"Title: {self.title}\n" \
+               f"Sheet Music: {self.score_img}\n" \
+               f"Text: {self.text}\n"
 
 
 if __name__ == '__main__':
@@ -89,17 +101,45 @@ if __name__ == '__main__':
     book.set_identifier('liederarchiv-liederbuch')
     book.set_title('Lieder Archiv - Liederbuch')
     book.set_language('de')
+    book.add_metadata('DC', 'description', 'Alle Lieder von www.lieder-archiv.de in einem Buch!')
+    book.add_metadata(None, 'copyright', 'Copyright © 1996-2020 Alojado Publishing')
 
     contents = OrderedDict()
 
     for char in "a":         # letters:
+        print(f"Fetching songs starting with {char.upper()}...")
+
         songs = get_song_list(get_list_url(char))
         per_letter_contents = []
 
-        for _song_url in songs:
+        for _song_url in ["https://www.lieder-archiv.de/alphabet_song-notenblatt_100505.html"]:     # songs:
             song = Song(_song_url)
-            per_letter_contents.append(song)
+
+            # Prepare image file
+            image = Image.open(song.score_img)
+            b = io.BytesIO()
+            image.save(b, 'png')
+            b_image = b.getvalue()
+            book.add_item(epub.EpubItem(uid=song.id, file_name=str(song.score_img), media_type='image/png', content=b_image))
+
+            chapter = epub.EpubHtml(title=song.title, file_name=song.filename, lang='de')
+            chapter.content = song.html
+
+            per_letter_contents.append(chapter)
 
         contents[char.upper()] = per_letter_contents
 
-    print(contents)
+    spine_list = []
+    section_tuple = []
+
+    for k, v in contents.items():
+        book.add_item(v[0])
+        spine_list.append(v[0])
+        section_tuple.append((epub.Section(k), v[0]))
+
+    book.toc = section_tuple
+    book.spine = spine_list
+    # book.add_item(epub.EpubNcx())
+    # book.add_item(epub.EpubNav())
+
+    epub.write_epub('Liederbuch.epub', book)
