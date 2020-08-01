43ec40feff96fd87f74c890091a7a0505dad754e
Conny Lin <cornelia.lin@tum.de>
2020-08-01 23:12:13 +0200

    Download image for a song done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from ebooklib import epub
+from pathlib import Path
 import requests
 
 BOOK = epub.EpubBook()
@@ -16,5 +17,35 @@ def get_song_list(list_url):
     return links
 
 
+def get_song_title(song_url):
+    soup = BeautifulSoup(requests.get(song_url).text, 'html.parser')
+    return soup.find('div', class_='heading').find('h2').text
+
+
+def _get_score_image_url(song_url):
+    soup = BeautifulSoup(requests.get(song_url).text, 'html.parser')
+    return soup.find('img', class_='score', src=True)['src']
+
+
+def save_score_image(song_url):
+    """
+    Saves the score PNG under ./score_images/
+    :param song_url: URL of the song page
+    :return: path to created file
+    """
+    song_url = _get_score_image_url(song_url)
+    folder = Path('./score_images')
+    folder.mkdir(parents=True, exist_ok=True)
+    out_path = folder / song_url.split('/')[-1]
+    response = requests.get(song_url)
+
+    with open(out_path, 'wb') as file:
+        file.write(response.content)
+
+    return out_path
+
+
 if __name__ == '__main__':
-    print(get_song_list(get_list_url('a')))
+    a_songs = get_song_list(get_list_url('a'))
+    katze = a_songs[3]
+    save_score_image(katze)
