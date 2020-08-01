39f09cff4ff55f19780d9a903290a2e917202c32
Conny Lin <cornelia.lin@tum.de>
2020-08-02 00:01:19 +0200

    Refactor for efficiency

--- main.py
+++ main.py
@@ -10,26 +10,42 @@ def get_list_url(letter):
     return f"https://www.lieder-archiv.de/lieder_sammlung_{letter}.html"
 
 
-def get_song_list(list_url):
-    soup = BeautifulSoup(requests.get(list_url).text, 'html.parser')
-    ul = soup.find('ul', class_='list')
+def _get_song_list(list_soup):
+    ul = list_soup.find('ul', class_='list')
     links = [a['href'] for a in ul.find_all('a', href=True)]
     return links
 
 
+def get_song_list(list_url):
+    soup = BeautifulSoup(requests.get(list_url).text, 'html.parser')
+    return _get_song_list(soup)
+
+
+def _get_song_title(song_soup):
+    return song_soup.find('div', class_='heading').find('h2').text
+
+
 def get_song_title(song_url):
     soup = BeautifulSoup(requests.get(song_url).text, 'html.parser')
-    return soup.find('div', class_='heading').find('h2').text
+    return _get_song_title(soup)
+
+
+def _get_song_text(song_soup):
+    return song_soup.find('div', class_='lyrics-container')
 
 
 def get_song_text(song_url):
     soup = BeautifulSoup(requests.get(song_url).text, 'html.parser')
-    return soup.find('div', class_='lyrics-container')
+    return _get_song_text(soup)
 
 
-def _get_score_image_url(song_url):
+def _get_score_image_url(song_soup):
+    return song_soup.find('img', class_='score', src=True)['src']
+
+
+def get_score_image_url(song_url):
     soup = BeautifulSoup(requests.get(song_url).text, 'html.parser')
-    return soup.find('img', class_='score', src=True)['src']
+    return _get_score_image_url(soup)
 
 
 def save_score_image(song_url):
@@ -38,7 +54,7 @@ def save_score_image(song_url):
     :param image_url: URL of the song page
     :return: path to created file
     """
-    image_url = _get_score_image_url(song_url)
+    image_url = get_score_image_url(song_url)
     folder = Path('./score_images')
     folder.mkdir(parents=True, exist_ok=True)
     out_path = folder / image_url.split('/')[-1]
@@ -53,10 +69,11 @@ def save_score_image(song_url):
 
 
 class Song:
-    def __init__(self, title, score_img, text):
-        self.title = title
-        self.score_img = score_img
-        self.text = text
+    def __init__(self, song_url):
+        song_soup = BeautifulSoup(requests.get(song_url).text, 'html.parser')
+        self.title = _get_song_title(song_soup)
+        self.score_img = save_score_image(song_url)
+        self.text = _get_song_text(song_soup)
 
     def __str__(self):
         return f"==== SONG ====" \
@@ -78,12 +95,11 @@ if __name__ == '__main__':
     for char in "a":         # letters:
         songs = get_song_list(get_list_url(char))
         per_letter_contents = []
-        for song_url in songs:
-            _title = get_song_title(song_url)
-            _score_img = save_score_image(song_url)
-            _text = get_song_text(song_url)
 
-            per_letter_contents.append(Song(_title, _score_img, _text))
+        for _song_url in songs:
+            song = Song(_song_url)
+            per_letter_contents.append(song)
+
         contents[char.upper()] = per_letter_contents
 
     print(contents)
